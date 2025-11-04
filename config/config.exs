c6aa5ba8756951d60e11a44d1da5cff5a4889b17
unknown <dkfkgks64@gmail.com>
2025-11-04 12:46:41 +0900

    feature, chore: ログ導入

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,11 @@
 import Config
 config :github_issues,
   github_url: "https://api.github.com"
+
+config :logger,
+  compile_time_purge_matching: [
+    [level_lower_than: :info]
+  ]
+config :logger, :default_formatter,
+  format: "$time $metadata[$level] $message\n", # デフォルトフォーマットでは最初に改行するのでそれを除く
+  colors: [info: :green]
--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -4,6 +4,7 @@ defmodule Issues.GithubIssues do
 
   公開プロジェクトのIssuesを照会する
   """
+  require Logger
 
   @github_url Application.compile_env(:github_issues, :github_url)
 
@@ -11,6 +12,8 @@ defmodule Issues.GithubIssues do
   コマンド入力に該当するプロジェクトのissuesを取得
   """
   def fetch(user, project) do
+    Logger.info("ユーザ#{user}のプロジェクト#{project}のissuesを取得")
+
     issues_url(user, project)
     |> Req.get!()
     |> handle_response()
@@ -20,11 +23,15 @@ defmodule Issues.GithubIssues do
     "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response(%Req.Response{status: 200, body: body}) do
-    {:ok, body}
+  def handle_response(%Req.Response{status: status, body: body}) do
+    Logger.info("レスポンス情報： ステータスコード=#{status}")
+    Logger.debug(fn -> inspect(body) end)
+    {
+      check_for_error(status),
+      body
+    }
   end
 
-  def handle_response(%Req.Response{status: _, body: body}) do
-    {:error, body}
-  end
+  defp check_for_error(200), do: :ok
+  defp check_for_error(_), do: :error
 end
