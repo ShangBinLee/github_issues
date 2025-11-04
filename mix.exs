0596c06f0c2ef7ef5fd1e7de23b4ac3d253612c2
unknown <dkfkgks64@gmail.com>
2025-11-04 12:48:28 +0900

    docs: ex_doc導入

--- mix.exs
+++ mix.exs
@@ -8,7 +8,13 @@ defmodule GithubIssues.MixProject do
       version: "0.1.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "GithubIssues",
+      source_url: "https://github.com/ShangBinLee/github_issues",
+      homepage_url: "https://github.com/ShangBinLee/github_issues",
+      docs: docs(),
     ]
   end
 
@@ -24,13 +30,21 @@ defmodule GithubIssues.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:req, "~> 0.5.0"}
+      {:req, "~> 0.5.0"},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false, warn_if_outdated: true},
     ]
   end
 
   defp escript_config do
     [
       main_module: Issues.CLI,
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Issues.CLI",
+      extras: ["README.md"]
     ]
   end
 end
